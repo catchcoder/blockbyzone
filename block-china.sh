3a7a181fbff00b3d0d8fce8aae60ee4ddffad163
Chris Hawkins <catchcoder@users.noreply.github.com>
2025-02-10 15:56:50 +0000

    Update block-china.sh

--- block-china.sh
+++ block-china.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+
 # Create the ipset list
 ipset -N china hash:net
 
-# remove any old list that might exist from previous runs of this script
-rm cn-aggregated.zone
+# Remove any old list that might exist from previous runs of this script
+rm -f cn-aggregated.zone
 
 # Pull the latest IP set for China
 wget -P . https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone
 
 # Add each IP address from the downloaded list into the ipset 'china'
-for i in $(cat /etc/cn-aggregated.zone ); do ipset -A china $i; done
+for i in $(cat ./cn-aggregated.zone ); do ipset -A china $i; done
 
-# Restore iptables
-/sbin/iptables-restore < /etc/iptables.firewall.rules
+COMMAND="netfilter-persistent"
+
+if which "$COMMAND" > /dev/null 2>&1; then
+    sudo netfilter-persistent save
+else
+    echo "iptables-persistent needs to be installed available."
+fi
