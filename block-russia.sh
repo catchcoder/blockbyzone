d386cd8942365ba71ac69bc32a70d20efac8768f
Chris Hawkins <catchcoder@users.noreply.github.com>
2025-02-10 15:58:26 +0000

    Update block-russia.sh

--- block-russia.sh
+++ block-russia.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
 # Create the ipset list
 ipset -N russia hash:net
 
-# remove any old list that might exist from previous runs of this script
-rm ru-aggregated.zone
+# Remove if existing zone downloaded
+rm -f ru-aggregated.zone
 
 # Pull the latest IP set for Russia
 wget -P . http://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone
 
 # Add each IP address from the downloaded list into the ipset 'russia'
-for i in $(cat /etc/ru-aggregated.zone ); do ipset -A russia $i; done
+for i in $(cat ./ru-aggregated.zone ); do ipset -A russia $i; done
 
-# Restore iptables
-/sbin/iptables-restore < /etc/iptables.firewall.rules
+COMMAND="netfilter-persistent"
+
+if which "$COMMAND" > /dev/null 2>&1; then
+    sudo netfilter-persistent save
+else
+    echo "iptables-persistent needs to be installed available."
+fi
