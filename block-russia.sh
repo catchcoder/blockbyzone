c4ddd1ace56f2f974b67415d85e9c9820feb6462
Chris Hawkins <catchcoder@users.noreply.github.com>
2025-02-11 15:17:33 +0000

    Update block-russia.sh

--- block-russia.sh
+++ block-russia.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Check if the script is running with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
 # Create the ipset list
 ipset -N russia hash:net
 
@@ -10,6 +17,9 @@ wget -P . http://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone
 
 # Add each IP address from the downloaded list into the ipset 'russia'
 for i in $(cat ./ru-aggregated.zone ); do ipset -A russia $i; done
+
+# Remove if existing zone downloaded
+rm -f ./ru-aggregated.zone
 
 COMMAND="netfilter-persistent"
 
