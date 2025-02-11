a877020aa2daaeb9edc18fb0fe51ac5fa05dca81
Chris Hawkins <catchcoder@users.noreply.github.com>
2025-02-11 15:15:59 +0000

    check is sudo

--- block-threats.sh
+++ block-threats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the script is running with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
 # Create the ipset list
 ipset -N emergetreats hash:net
 
