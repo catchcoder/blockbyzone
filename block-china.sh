3cd2777079008204efdf7bacc8dd990f6a392c74
Chris Hawkins <catchcoder@users.noreply.github.com>
2025-02-11 15:20:24 +0000

    Update block-china.sh

--- block-china.sh
+++ block-china.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+# Check if the script is running with sudo
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
+# Cleanup
+removedldfile() {
+    # Remove if file exist
+    rm -f ./cn-aggregated.zone
+}
+
+removedldfile
+
 # Create the ipset list
 ipset -N china hash:net
 
-# Remove any old list that might exist from previous runs of this script
-rm -f ./cn-aggregated.zone
+
 
 # Pull the latest IP set for China
 wget -P . https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone
 
 # Add each IP address from the downloaded list into the ipset 'china'
 for i in $(cat ./cn-aggregated.zone ); do ipset -A china $i; done
+
+removedldfile
 
 COMMAND="netfilter-persistent"
 
